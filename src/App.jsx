ddc3fd13ca82c9fb9c3ffcf13b282f8d2ca26f3b
vanessa-tm <vanessatmiller@gmail.com>
2024-12-12 11:47:57 -0700

    header and footer initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.scss'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Header />
+      <Footer />
     </>
   )
 }
