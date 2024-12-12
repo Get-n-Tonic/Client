4c29ddedaa7be93fd78c964dd92422e297fc7312
vanessa-tm <vanessatmiller@gmail.com>
2024-12-12 13:34:46 -0700

    create breakpoints styling for header and footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,18 +11,19 @@ return (
 <>
     <footer className='footer'>
 
-        <h1 className='footer__logo'>Christmas Cocktail Creations</h1>
-        <div className='footer__containter'>
+        <h1 className='footer__logo'>Christmas Cocktail Creations &#x1F378;</h1>
+        <div className='footer__container'>
             <div className='footer__social-media'>
                 <img src={facebookIcon} />
                 <img src={pinterestIcon}/>
                 <img src={instagramIcon}/>
             </div>
-            <p>Mix, Sip, Celebrate!</p>
+            <p className='footer__catchphrase'>Mix, Sip, Celebrate!</p>
+            <p className='footer__copyright'>© 2024 Cocktail Creations. All Rights Reserved.</p>
         </div>
        
         
-        <p>© 2024 Cocktail Creations. All Rights Reserved.</p>
+        
     </footer>
    
 
